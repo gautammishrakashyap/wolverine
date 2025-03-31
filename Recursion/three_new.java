061836e22732b87503e4259acfc938fd1fc509c2
gautammishrakashyap <kumargautam7071@gmail.com>
2025-03-31 16:35:31 +0530

    recurrsion

---
+++ Recursion/three_new.java
@@ -0,0 +1,15 @@
+public class three_new {
+    public static void main(String[] args) {
+        int n = 8;
+        if (n==0) {
+            System.out.println(0);
+            
+        }
+        else if(n==1){
+            System.out.println(1);
+        }
+        else {
+            System.out.println((n-1)+(n-2));
+        }
+    }
+}
